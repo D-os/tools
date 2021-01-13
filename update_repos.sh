5173bdcf82d68b872c6fa8499afdab4aa97c9122
Tomasz Sterna <tomasz@sterna.link>
2021-01-13 18:32:35 +0100

    update_repos.sh docs and checkout

--- update_repos.sh
+++ update_repos.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 set -e
+### This tool splits Android repositories to separate libraries.
+### It will checkout given Android release tag, filter-out specific directory
+### and then push library to separate repository tag, to rebase code over.
+### For available Android branches see:
+### https://source.android.com/setup/start/build-numbers.html#source-code-tags-and-builds
 
-[ $# -lt 1 ] && { echo "Usage: `basename $0` android_branch" >&2; exit 1; }
+[ $# -lt 1 ] && { echo -e "Usage: `basename $0` android_branch\n" >&2; sed -n '/^### /s/^### //p' "$0" >&2; exit 1; }
 
 ANDROID_BRANCH=$1
-echo Will update repos to $ANDROID_BRANCH \( https://source.android.com/setup/start/build-numbers.html#source-code-tags-and-builds \)
+echo Will update repos to $ANDROID_BRANCH
 echo
 
 
 BASE=`dirname $0`
 BASE=`readlink -f $BASE`
 cd $BASE
+
+function check_repo() {
+	REPO=$1
+	SRC=$2
+	if [ -d $REPO ]; then
+		[ -d $REPO/.git ] || { echo "Not a git repository: $BASE/$REPO" >&2; exit 2; }
+	else
+		git clone $SRC $REPO
+	fi
+}
 
 function update_repo() {
 	REPO=$1
@@ -26,6 +41,11 @@ function update_repo() {
 	[ -n "$DIR" ] && git filter-branch --force --prune-empty --subdirectory-filter $DIR HEAD
 	git push $DEST HEAD:refs/tags/$ANDROID_BRANCH
 }
+
+check_repo system-core https://android.googlesource.com/platform/system/core
+check_repo frameworks-native https://android.googlesource.com/platform/frameworks/native
+check_repo system-tools-aidl https://android.googlesource.com/platform/system/tools/aidl
+check_repo system-tools-hidl https://android.googlesource.com/platform/system/tools/hidl
 
 update_repo system-core base git@github.com:D-os/base.git
 update_repo system-core libcutils git@github.com:D-os/libcutils.git
